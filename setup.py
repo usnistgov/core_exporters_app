92dd8d454b821187a274cfd2ddee16dc7e61f568
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-06-12 14:15:59 -0400

    chore: upgrade 1.3.0
    Change-Id: I9a3c834e42b1fd54628ace634b9c8746c3917c09

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_exporters_app",
-    version="1.3.0.dev1",
+    version="1.3.0",
     description="Exporters for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
