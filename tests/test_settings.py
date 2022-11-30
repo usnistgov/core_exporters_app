b815154493a3a811ac692897c82ec0a778c9031a
Philippe Dessauw <philippe.dessauw@nist.gov>
2022-11-30 11:48:40 -0500

    chore: bump xml_utils version and fix test settings
    Change-Id: Ie53710708ba126b57733134d9b2763c74dc67eb2

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -56,3 +56,6 @@ DATABASES = {
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 CAN_ANONYMOUS_ACCESS_PUBLIC_DOCUMENT = False
 CELERYBEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+MONGODB_INDEXING = False
+MONGODB_ASYNC_SAVE = False
